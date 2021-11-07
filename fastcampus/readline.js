e4efcb8cea3619e718e4a30825423a03bb1a88ee
NANGGI <mqsora@naver.com>
2021-11-07 18:47:39 +0900

    Add files via upload

--- fastcampus/readline.js
+++ fastcampus/readline.js
@@ -9,18 +9,24 @@ rl.question('원하는 도형을 작성해주세요. ( 정사각형, 원 ) : ', 
 
   switch (figure) {
     case "정사각형":
-      rl.question("변의 길이를 입력 해주세요.", input => {
+      rl.question("변의 길이를 입력 해주세요 : ", input => {
         console.log(`입력받은 값 : ${input}`);
         console.log(`정사각형의 넓이는 : ${input * input} 입니다.` );
         rl.close();
       });
       break;
-  
     case "원":
-      
+      rl.question("반지름의 길이를 입력 해주세요 : ", input => {
+        console.log(`입력받은 값 : ${input}`);
+        console.log(`원의 넓이는 : ${input * input * 3.14} 입니다.` );
+        rl.close();
+      });
       break;
-
     default:
+      console.log(
+        "지원되지 않는 도형입니다. '정사각형' 또는 '원'을 입력해주세요. \n커맨드 라인을 종료합니다."
+      );
+      rl.close();
       break;
   }
 
